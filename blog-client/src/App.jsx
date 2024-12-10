982d6fd997348bbdb99b3b8fe13a2a5ccb5178d0
srm04072024 <s.r.m.20.3.2018@gmail.com>
2024-12-11 02:53:33 +0530

    chore:Initialising Blog-Project & install tailwind CSS

---
+++ blog-client/src/App.jsx
@@ -0,0 +1,9 @@
+// import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return <div className=" text-3xl text-red-700 text-center">Hello world</div>;
+}
+
+export default App;
